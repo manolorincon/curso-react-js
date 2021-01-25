1d519fa29c28edf2a2c0ad6edb6768225d021a6c
Manuel Rincon <manolorincon26@gmail.com>
2021-01-25 07:37:05 -0300

    ultimos cambios

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -12,6 +12,7 @@ import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
 import { PrivateRoute } from "./PrivateRoute";
 import { PublicRoute } from "./PublicRoute";
+import { startLoadingNotes } from '../actions/notes';
 
 export const AppRouter = () => {
 
@@ -25,11 +26,12 @@ export const AppRouter = () => {
     useEffect(() => {
 
         //firebase me indica cuando el estado de la autenticacion cambia
-        firebase.auth().onAuthStateChanged( (user) => {
+        firebase.auth().onAuthStateChanged( async (user) => {
 
             if ( user?.uid ) {
                 dispatch(login( user.uid, user.displayName ));
                 setIsLoggedIn(true);
+                dispatch(startLoadingNotes(user.uid));
             }else{
                 setIsLoggedIn(false);
             }
